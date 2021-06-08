5870aad2489890f084cd2409246580610675b6cd
TaewanKimmmm <ktww88@gmail.com>
2021-06-09 00:42:30 +0900

    build: 환경 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,8 @@
+DROP table if exists SECTION;
+DROP table if exists STATION;
+DROP table if exists LINE;
+DROP table if exists MEMBER;
+
 create table if not exists STATION
 (
     id bigint auto_increment not null,
